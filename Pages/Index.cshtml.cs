255609fdfb3eac127d405461538b530a052ef7ee
User <User@DESKTOP-IDMA6O3>
2025-11-15 21:11:29 +0200

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Orosz_Gyozo_Lab_1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
